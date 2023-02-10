a3ff5af63fea631acdfebd8414224f5453323f5e
Arman Zadehattar <arman.zattar@gmail.com>
2023-02-09 21:13:36 -0500

    Initial Commit

---
+++ NoteBook/NoteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteBookApp.swift
+//  NoteBook
+//
+//  Created by Arman Zadeh-Attar on 2023-02-09.
+//
+
+import SwiftUI
+
+@main
+struct NoteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
